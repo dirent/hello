b3697a2b57258395690597c6f347975b7c8094be
Dirk Entgelmeier <d.entgelmeier@ceyoniq.com>
2022-03-29 17:13:19 +0200

    Introduce first class

--- src/app2.js
+++ src/app2.js
@@ -1,1 +1,12 @@
-console.log("Hello Web Standards")
+class App {
+    constructor() {
+        console.log("created",this);
+    }
+
+    init() {
+        console.log("initialized");
+    }
+}
+
+const app = new App();
+app.init();
