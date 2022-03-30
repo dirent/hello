a6ee490e53726d3e0cb23a372db390de91640764
Dirk Entgelmeier <d.entgelmeier@ceyoniq.com>
2022-03-30 15:46:07 +0200

    Use a global function

--- src/app2.js
+++ src/app2.js
@@ -6,20 +6,15 @@ class App {
 
     init() {
         console.log("initialized",this.message);
+        hello(this.message);
     }
 }
 
-const app = new App();
-app.init();
+console.log('load time');
+
+function hello(greeting) {
+    console.log('global',greeting);
+}
 
 const withParams = new App('java');
 withParams.init();
-
-const undef = new App(undefined);
-undef.init();
-
-const withNull = new App(null);
-withNull.init();
-
-const withEmptyString = new App('');
-withEmptyString.init();
