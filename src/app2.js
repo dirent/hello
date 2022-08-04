3c43260e0520a5c8333ecf2bb8b281ef91af4071
dirk <dirk@gmx.org>
2022-08-04 17:43:55 +0200

    Add demo for class inheritance

--- src/app2.js
+++ src/app2.js
@@ -1,10 +1,14 @@
-import Text from './text.js'
+import Text from './text.js';
+import Input from './input.js';
 
 class App {
     constructor(message='duke') {
         this.message = message;
         this.first = new Text('first');
         this.second = new Text('second');
+        this.firstInput = new Input('firstInput', function(e) {
+            console.log(e.target.value);
+        });
         this.init();
     }
 
@@ -14,7 +18,7 @@ class App {
     }
 }
 
-console.log('load time');
+console.log('loading...');
 
 const sayHello = greeting => console.log('global',greeting);
 sayHello('chief duke');
